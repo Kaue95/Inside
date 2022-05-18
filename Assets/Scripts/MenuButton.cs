f0ef520637a0175854aaad3d5129120a4c12b883
Kaue95 <Kaue95@outlook.com>
2022-05-17 22:12:51 -0300

    telav3

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -11,11 +11,15 @@ public class MenuButton : MonoBehaviour
     public GameObject Options;
     public GameObject Slots;
     private bool slots = false;
+    private bool sair = false;
+    private bool sairconfirm = false;
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            
+            sair = false;
             slots = false;
             Main.SetActive(false);
             Options.SetActive(true);
@@ -24,6 +28,8 @@ public class MenuButton : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
+           
+            sair = true;
             slots = false;
             Main.SetActive(true);
             Options.SetActive(false);
@@ -31,11 +37,13 @@ public class MenuButton : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.UpArrow))
         {
+           
+            sair = false;
             slots = true;
             Slots.SetActive(slots);
             Options.SetActive(false);
             Main.SetActive(false);
-           
+
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && slots == true)
@@ -43,7 +51,20 @@ public class MenuButton : MonoBehaviour
             SceneManager.LoadScene(1);
         }
 
+        if (Input.GetKey(KeyCode.DownArrow) && sair == true)
+        {
+            Debug.LogWarning("Saindo");
+
+
         }
 
     }
+}
+    
 
+
+/*else if (Input.GetKey(KeyCode.DownArrow) && sairconfirm == true)
+        {
+    Debug.LogWarning("Saindo...");
+    Application.Quit();
+    wait*/
