bdc7a1a2ea760f9bbee3bbfd01cdb5570803b526
Kaue95 <Kaue95@outlook.com>
2022-05-26 18:46:25 -0300

    Barra de carregamento (teste2)

--- Assets/Scripts/LoadingBar.cs
+++ Assets/Scripts/LoadingBar.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
 
 public class LoadingBar : MonoBehaviour
 {
-  
-    void Start()
+    private Slider progressbar;
+    public float Speed = 0.01f;
+    private float progressoatual = 0;
+    private float seg = 0.5f;
+    private void Awake()
+    {
+        progressbar = gameObject.GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        addprogress(0.75f);
+            if(progressoatual == 2.0f)
+        {
+            SceneManager.LoadScene(1);
+        }
+       
+    }
+
+    private void Update()
     {
         
+        if (progressbar.value < progressoatual)
+        {
+            
+            progressbar.value += Speed = Time.deltaTime;
+        }
+
+
+    }
+
+    //adicionando barra de progresso na barra 
+    public void addprogress(float newprogress)
+    {
+       
+        progressoatual = progressbar.value += newprogress;
+       
     }
 
     
-    void Update()
-    {
-        
-    }
 }
