8a2f393c25177ae4e35b04e8ca0bbfbfb35a81c9
Kaue95 <Kaue95@outlook.com>
2022-05-27 20:23:24 -0300

    Barra de carregamento funcional

--- Assets/Scripts/LoadingBar.cs
+++ Assets/Scripts/LoadingBar.cs
@@ -9,33 +9,60 @@ public class LoadingBar : MonoBehaviour
 {
     private Slider progressbar;
     public float Speed = 0.01f;
+    private float tempo = 0.2f;
     private float progressoatual = 0;
-    private float seg = 0.5f;
+
     private void Awake()
     {
         progressbar = gameObject.GetComponent<Slider>();
+        StartCoroutine(Carrega());
     }
 
-    private void Start()
+    IEnumerator Carrega()
     {
-        addprogress(0.75f);
-            if(progressoatual == 2.0f)
+        if(progressbar.value != 2.0f) { 
+            while (progressbar.value != 2.0f)
+            {
+                if (progressbar.value < 0.5f)
+                {
+                   
+                    tempo = 0.2f;
+                    
+                }else if(progressbar.value > 0.5f && progressbar.value < 0.7f)
+                {
+                    Debug.Log("IF 1");
+                    tempo = 1.0f;
+                    
+                }else if (progressbar.value > 1.0f && progressbar.value < 1.5f)
+                {
+                    Debug.Log("IF 1");
+                    tempo = 1.0f;
+
+                }
+
+
+
+
+
+
+
+
+                progressbar.value += 0.02f;
+                yield return new WaitForSeconds(tempo);
+
+            }
+        }
+       
+       if(progressbar.value == 2)
         {
             SceneManager.LoadScene(1);
         }
-       
-    }
 
+        
+    }
     private void Update()
     {
-        
-        if (progressbar.value < progressoatual)
-        {
-            
-            progressbar.value += Speed = Time.deltaTime;
-        }
-
-
+     
     }
 
     //adicionando barra de progresso na barra 
